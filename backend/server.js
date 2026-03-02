fd7c696710b90bad1cc4da3114af58b43a33fd92
chaitalikharade27 <chaitalikharade27@gmail.com>
2026-03-02 19:11:10 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,10 @@
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running");
+});
+const cors = require("cors");
+app.use(cors());
+
 import dotenv from "dotenv";
 dotenv.config();
 
